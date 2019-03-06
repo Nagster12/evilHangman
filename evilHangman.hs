df31af49a131a89d990d4a6bc5705f35a333e620
keegan0905 <keegan0905@gmail.com>
2019-03-06 12:00:55 -0500

    I did: Some basic main IO stuff is done TODO: more argument checking functions need to be done Incomplete: Any portion of the game

--- evilHangman.hs
+++ evilHangman.hs
@@ -10,21 +10,23 @@ import Data.Char
 main = do
   args <- getArgs
   -- Check argument Validity
-  checkArgs args
-
+  let validateMsg = checkArgs args
+  let mode        = checkDebugMode args
+  if validateMsg /= "" putStr validateMsg -- An Error has occurred
+  else --TODO Parse all the arguments and pass it into the play function
+  (playHangman dictionary wordLength guessCount mode)
   
 checkArgs :: [String] -> String
 checkArgs args
-  | validWordLength (getWordLength args) = "Error: A word of that length does not exist in my Dictionary"
-  | validGuess (getGuessCount args) == 4 && (last args) == "-n" = putStr (playHangmanDebug args)
-  | (length args) == 3                        = putStr (playHangmanNormal args)
-  | otherwise = "error"
-  else putStr "error"
-  --[dictFile, wordLength, guessCount, debugN]
-  --putStr inputLength
-  
-playHangmanNormal:: [String] -> String
-playHangmanNormal args = "Normal"
+  | badFileName   (getFileName   args) = "Error: The dictionary does not exist"
+  | badWordLength (getWordLength args) = "Error: A word of that length does not exist in my Dictionary"
+  | badGuessCount (getGuessCount args) = "Error: You must select a number of guesses larger than 1 and less than 26"
+  | otherwise = ""
 
-playHangmanDebug:: [String] -> String
-playHangmanDebug args = "Debug"
+checkDebugMode [String] -> Bool
+checkDebugMode args
+  | (length args) == 4 && (last args) == "-n" = True
+  | (length args) == 3                        = False
+
+playHangman:: [String] -> Int -> Int -> Bool
+playHangman args = True
