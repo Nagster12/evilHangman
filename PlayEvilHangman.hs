1f96d142a9fd289e7abf1361aa101b29e90e4ff5
keegan0905 <keegan0905@gmail.com>
2019-03-17 10:45:54 -0400

    Keegan did - IO stuff: Most of the game IO is done, need to work out the kinks with the final steps of the game when I can put together Noahs stuff

--- PlayEvilHangman.hs
+++ PlayEvilHangman.hs
@@ -6,8 +6,76 @@ module PlayEvilHangman where
 import System.Environment
 import System.IO
 import Data.Char
+import Control.Monad (when)
 
---playEvilHangman:: [String] -> Int -> Int -> Int
-playEvilHangman dictionary wordLength guessCount = do
-  putStr "it works"
-  putStr (show wordLength ++ show guessCount ++ show dictionary) -- DEBUG: Shows all inputs
+-- playEvilHangman: The game begins! with the user selected options we will play evil hangman
+playEvilHangman :: [String] -> Int -> Int -> Bool -> IO ()
+playEvilHangman dictionary wordLength guessCount isDebugMode = do
+  -- Initial Conditions of variables
+  let familyPattern = [1,3]
+  let guessedLetters = []
+  let hangmanWord = []
+  -- Iterate game state till game is over
+  recursiveHangman dictionary wordLength guessCount isDebugMode guessedLetters familyPattern hangmanWord
+  
+  
+recursiveHangman :: [String] -> Int -> Int -> Bool -> [Char] -> [Int] -> String -> IO ()
+recursiveHangman dictionary wordLength guessCount isDebugMode guessedLetters familyPattern hangmanWord = do
+  -- Show user current Game State Info
+   -- step 1
+  putStrLn ("Guesses Left: " ++ show guessCount)
+  -- step 2
+  putStrLn ("Letters Guessed: " ++ show guessedLetters)
+  -- step 3
+  putStrLn ("Word so far: " ++ showHangmanWord hangmanWord guessedLetters familyPattern)
+  putStrLn "Guess?:"
+  -- step 4
+  debugPrint (currFamilyPatternCount familyPattern) isDebugMode
+  -- step 5:
+  -- Request user to guess for a letter
+  eof <- hIsEOF stdin
+  -- Check for eof
+  if eof 
+    then return ()
+    else do
+      -- Get that letter as inputChar
+      inputLine <- hGetLine stdin
+      let inputChar = head inputLine
+      if (((not.isAlpha) inputChar) || (elem inputChar guessedLetters)) -- the user's char has to be an unused letter
+        then do -- Get the user to enter a new input
+        putStrLn "\nError: Invalid Guess: Please choose a unique letter"
+        recursiveHangman dictionary wordLength guessCount isDebugMode guessedLetters familyPattern hangmanWord
+        else do
+        -- Step 6
+        -- Partion dictionary with the inputChar (TODO: Noah)
+        -- Create a newDict *important for step 9*
+        let newDict = ["new", "dictionary"]
+        -- Step 7
+        -- Update familyPattern (TODO: Noah)
+        -- step 8 update guessCount when input char is not in the dictionary
+        let newGuessCount = if (not (charInDict inputChar newDict))
+                            then guessCount - 1
+                            else guessCount
+        -- step 9
+        when (newGuessCount == 0) (putStrLn "You're out of Guesses. Game Over")
+        -- step 10 
+        putStrLn "got to this point."
+
+-- Helper Functions:
+--TODO FINISH ShowHangman
+showHangmanWord :: String -> [Char] -> [Int] -> String
+showHangmanWord hangmanWord guessedletters familyPattern = "fake hangman word"
+
+-- currDictFamilyCount (I think it is finished)
+currFamilyPatternCount :: [Int] -> Int
+currFamilyPatternCount currPattern = length currPattern
+
+debugPrint :: Int -> Bool -> IO ()
+debugPrint patternCount isDebugMode = do
+  if isDebugMode then putStr (show patternCount) else (return ())
+  
+charInDict :: Char -> [String] -> Bool
+charInDict _ [] = False
+charInDict char (x:xs) 
+  | elem char x = True
+  | otherwise   = charInDict char xs 
